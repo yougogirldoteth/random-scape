cba255838040ff722910760413a5be3c7f6504e4
yougogirldoteth <name.yougogirl@web.de>
2024-01-27 23:38:18 +0000

    first commit

--- src/pages/api/randomscape.ts
+++ src/pages/api/randomscape.ts
@@ -1,13 +1,13 @@
-// pages/api/basepaint.ts
+// pages/api/randomscape.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   // Generate a random number, for example between 1 and 100
-  const randomId = Math.floor(Math.random() * 172) + 1;
+  const randomId = Math.floor(Math.random() * 9999) + 1;
 
   // Append the random number to the post URL query parameter
-  const postUrl = `https://frame-test-three.vercel.app/api/basepaint`;
-  const image_url = `https://basepaint.xyz/api/art/image?day=${randomId}`;
+  const postUrl = `https://random-scape.vercel.app/api/scapes`;
+  const image_url = `https://cdn.scapes.xyz/scapes/lg/${randomId}.png`;
 
 
   const body = `
@@ -20,7 +20,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         <meta property='og:image' content=${image_url} />
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="${image_url}" />
-        <meta property="fc:frame:button:1" content="Roll" />
+        <meta property="fc:frame:button:1" content="Gib random Scape" />
         <meta property="fc:frame:post_url" content="${postUrl}" />
       </head>
     </html>
