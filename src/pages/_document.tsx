be7e42a16aae62828d26168a1d703502b64e5879
onnnnnnnion <nounalysis@gmail.com>
2024-01-27 14:30:56 -0500

    change image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" />
+        <meta property="fc:frame:image" content="https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg" />
         <meta property="fc:frame:button:1" content="Green" />
         <meta property="fc:frame:button:2" content="Purple" />
         <meta property="fc:frame:button:3" content="Red" />
