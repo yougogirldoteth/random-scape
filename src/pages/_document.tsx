3c7d1d31521f02a3e135cbefb7ad24f8cbeb4b3a
onnnnnnnion <nounalysis@gmail.com>
2024-01-27 14:02:47 -0500

    add metadata tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630" />
+        <meta property="fc:frame:button:1" content="Green" />
+        <meta property="fc:frame:button:2" content="Purple" />
+        <meta property="fc:frame:button:3" content="Red" />
+        <meta property="fc:frame:button:4" content="Blue" />
+      </Head>
       <body>
         <Main />
         <NextScript />
