e4fe6116092756724d888d336f5d51bb9a078d82
yougogirldoteth <name.yougogirl@web.de>
2024-01-29 11:13:17 +0000

    add tiny dinos

---
+++ src/pages/api/randomtinydino.ts
@@ -0,0 +1,31 @@
+// pages/api/randomscape.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const randomId = Math.floor(Math.random() * 9999) + 1;
+    const postUrl = `https://random-scape.vercel.app/api/randomscape`;
+    const scaledImageUrl = `https://random-scape.vercel.app/api/scaled_tinydino/${randomId}`;
+
+    const body = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8"/>
+          <meta name="viewport" content="width=device-width"/>
+          <meta property="og:title" content="Frame" />
+          <meta property="og:image" content="${scaledImageUrl}" />
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:image" content="${scaledImageUrl}" />
+          <meta property="fc:frame:button:1" content="Gib random Scape" />
+          <meta property="fc:frame:post_url" content="${postUrl}" />
+        </head>
+      </html>
+    `;
+
+    res.status(200).setHeader('Content-Type', 'text/html').send(body);
+  } catch (error) {
+    console.error('Error generating random scape:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
---
+++ src/pages/api/scaled_tinydino/[id].ts
@@ -0,0 +1,61 @@
+// pages/api/scaled_square/[id].ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createCanvas, loadImage } from '@napi-rs/canvas';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    const { id } = req.query;
+    const imageUrl = `https://ipfs.io/ipfs/QmVCLxmzMstWpnCE2caF3AfRNEJmdiA6Z6a8BECnXqd98r/${id}.png`;
+
+    // Load the original square image using @napi-rs/canvas
+    const originalImage = await loadImage(imageUrl);
+
+    // Set the desired overall aspect ratio for the square image
+    const targetAspectRatio = 1.91 / 1;
+
+    // Calculate the new dimensions while maintaining the target aspect ratio
+    let newWidth = Math.round(originalImage.height * targetAspectRatio);
+    let newHeight = originalImage.height;
+
+    // Create a canvas with the new dimensions using @napi-rs/canvas
+    const canvas = createCanvas(newWidth, newHeight);
+    const context = canvas.getContext('2d');
+
+    // Draw a background with a color of your choice
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, newWidth, newHeight);
+
+    // Calculate the position to center the original square image on the canvas
+    const x = (newWidth - originalImage.width) / 2;
+
+    // Draw the original square image on the canvas
+    context.drawImage(originalImage, x, 0, originalImage.width, newHeight);
+
+    // Add Scape ID as text with Google Fonts on the left side
+    const text = `Tiny Dino #${id}`;
+    context.fillStyle = 'white';
+    context.font = 'bold 60px "Roboto Mono", sans-serif'; // Adjust font size and family
+    const textMetrics = context.measureText(text);
+    const textX = 40; // Adjust the left margin as needed
+    const textY = newHeight - 10;
+
+    context.fillText(text, textX, textY);
+
+    // Convert the canvas to a Buffer containing the PNG image
+    const buffer = canvas.toBuffer('image/png');
+
+    // Set the Content-Type header to indicate PNG image
+    res.setHeader('Content-Type', 'image/png');
+
+    // Send the image data as the response
+    res.status(200).send(buffer);
+  } catch (error) {
+    console.error('Error generating scaled square:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
