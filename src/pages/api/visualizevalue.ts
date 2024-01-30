b31859046cf82ebe560f5285d29a993c5fdd9b4c
yougogirldoteth <name.yougogirl@web.de>
2024-01-30 20:12:41 +0100

    add vv rare

---
+++ src/pages/api/visualizevalue.ts
@@ -0,0 +1,29 @@
+// pages/api/randomcheck.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Generate a random number, for example between 1 and 100
+  const randomId = Math.floor(Math.random() * 45) + 1;
+
+  // Append the random number to the post URL query parameter
+  const postUrl = `https://random-scape.vercel.app/api/visualizevalue`;
+  const image_url = `public/images_vv_rare/${randomId}.png`;
+
+
+  const body = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charSet="utf-8"/>
+        <meta property="og:title" content="Frame" />
+        <meta property='og:image' content=${image_url} />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="${image_url}" />
+        <meta property="fc:frame:button:1" content="visualizevalue" />
+        <meta property="fc:frame:post_url" content="${postUrl}" />
+      </head>
+    </html>
+  `;
+
+  res.status(200).setHeader('Content-Type', 'text/html').send(body);
+}
